ce7d20cbc1a499566e6a42d30b4c10437c1f5b69
Cesar Gimenes <crg@crg.eti.br>
2017-03-18 18:42:01 -0300

    input test

--- pcloader/main.go
+++ pcloader/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 var square *ebiten.Image
@@ -37,6 +39,41 @@ func update(screen *ebiten.Image) error {
 	screen.Fill(color.NRGBA{0x00, 0x00, 0xff, 0xff})
 
 	block(screen)
+
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		ebitenutil.DebugPrint(screen, "You're pressing the 'UP' button.")
+	}
+	// When the "down arrow key" is pressed..
+	if ebiten.IsKeyPressed(ebiten.KeyDown) {
+		ebitenutil.DebugPrint(screen, "\nYou're pressing the 'DOWN' button.")
+	}
+	// When the "left arrow key" is pressed..
+	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
+		ebitenutil.DebugPrint(screen, "\n\nYou're pressing the 'LEFT' button.")
+	}
+	// When the "right arrow key" is pressed..
+	if ebiten.IsKeyPressed(ebiten.KeyRight) {
+		ebitenutil.DebugPrint(screen, "\n\n\nYou're pressing the 'RIGHT' button.")
+	}
+
+	// When the "left mouse button" is pressed...
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		ebitenutil.DebugPrint(screen, "You're pressing the 'LEFT' mouse button.")
+	}
+	// When the "right mouse button" is pressed...
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonRight) {
+		ebitenutil.DebugPrint(screen, "\nYou're pressing the 'RIGHT' mouse button.")
+	}
+	// When the "middle mouse button" is pressed...
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonMiddle) {
+		ebitenutil.DebugPrint(screen, "\n\nYou're pressing the 'MIDDLE' mouse button.")
+	}
+
+	x, y := ebiten.CursorPosition()
+
+	// Display the information with "X: xx, Y: xx" format
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("X: %d, Y: %d", x, y))
+
 	return nil
 }
 
