0b0d4d05b2a6d0ed73f1da29fbe11413bc2c9181
Cesar Gimenes <crg@crg.eti.br>
2017-03-28 21:54:54 -0300

    add lex

--- goBASIC/basic.go
+++ goBASIC/basic.go
@@ -1,11 +1,40 @@
 package basic
 
-// Instance of the current BASIC machine.
-type Instance struct {
-	CurrentLine int
+import (
+	"fmt"
+	"io"
+	"strings"
+
+	"github.com/crgimenes/lex"
+)
+
+// TokenParsers array
+var TokenParsers = []lex.TokenFunction{
+	lex.Ident,
+	lex.NewLine,
+	lex.NotImplemented,
 }
 
 // Parse current code
-func (b *Instance) Parse() (err error) {
+func Parse() (err error) {
+
+	code := `10 print "test"
+20 goto 10`
+
+	var lex lex.Lexer
+	lex.MaxParseID = len(TokenParsers)
+
+	err = lex.Run(strings.NewReader(code))
+	if err != nil {
+		if err != io.EOF {
+			println(err.Error())
+			return
+		}
+	}
+
+	for _, t := range lex.Tokens {
+		fmt.Printf("%v\t%q\n", t.Type, t.Literal)
+	}
+
 	return
 }
