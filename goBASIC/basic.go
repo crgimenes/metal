8d64f30bf6c598f7d28b17d0f6810c0ed7d72f94
Cesar Gimenes <crg@crg.eti.br>
2017-03-28 21:20:31 -0300

    package draft

---
+++ goBASIC/basic.go
@@ -0,0 +1,11 @@
+package basic
+
+// Instance of the current BASIC machine.
+type Instance struct {
+	CurrentLine int
+}
+
+// Parse current code
+func (b *Instance) Parse() (err error) {
+	return
+}
